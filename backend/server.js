323f27a441361e09be9d9e59f96fbc2bf5d3d8a5
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-03-26 22:22:11 +0300

    create a route

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const workoutRoutes = require("./routes/workout");
 
-// Create express app
+//Express app
 const app = express();
 
-// Middleware 
+// Middleware
+app.use(express.json());
 app.use((req, res, next) => {
-  console.log(`Method: ${req.method}, Path: ${req.path}`); 
-  next(); 
+  console.log(`Method: ${req.method}, Path: ${req.path}`);
+  next();
 });
 
-// Define the root route
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to MERN Stack" });
-});
-//listen on port
+// Import routes
+app.use("/api/workouts", workoutRoutes);
+
+// Listen on port
+
 app.listen(process.env.PORT, () => {
-  console.log("Server listening on port", process.env.PORT);
+  console.log("Server is running on port", process.env.PORT);
 });
