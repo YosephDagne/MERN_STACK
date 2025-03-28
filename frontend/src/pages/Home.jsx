ca7396d1e957cd9fb44dd37b1d593faa447be9a9
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-03-28 15:52:52 +0300

    fetch data

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect  , useState } from 'react';
 
 const Home = () => {
+
+  const [workouts, setWorkouts] = useState([]);
+  useEffect(()=>{
+
+    const fetchWorkouts = async () => { 
+      const response = await fetch('/api/workouts');
+      const data = await response.json();
+      if(response.ok){
+        setWorkouts(data);
+      }
+      
+      
+    }
+    fetchWorkouts()
+  }, [])
   return (
     <div>
-      <h2>HOME</h2>
+      <div>
+        {
+          workouts && workouts.map((workout) => (
+            <p key={workout._id}>{ workout.title}</p>
+         ))
+        }
+      </div>
     </div>
   )
 }
